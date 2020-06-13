d0089b46dd7f2bdd5eb4cbb8da6fff68e829b0c6
budingxie <3257632906@qq.com>
2020-06-13 20:46:27 +0800

    init-project

---
+++ src/main/java/com/py/usedubbo/UseDubboApplication.java
@@ -0,0 +1,13 @@
+package com.py.usedubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseDubboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UseDubboApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/py/usedubbo/UseDubboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.py.usedubbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseDubboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
